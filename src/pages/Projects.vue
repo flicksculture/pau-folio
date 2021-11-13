7d8d4191b1eed212632c8052403b1da395386276
flicksculture <74396185+flicksculture@users.noreply.github.com>
2021-11-13 11:36:19 -0800

    Update Projects.vue

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -9,14 +9,14 @@
         <expandable-image src="https://i.ibb.co/fGP5s7M/2.jpg"></expandable-image>
         <expandable-image src="https://i.ibb.co/CQrJPLk/3.jpg"></expandable-image>
         <expandable-image src="https://i.ibb.co/fFspc9C/4.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/ZMg90JZ/5.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/b3LDsqF/6.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/RcL6C55/7.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/s6dT49C/8.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/wzr8zdk/8.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/QdYR8yy/10.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/hWkhf4M/11.jpg"></expandable-image>
-        <expandable-image src="https://ibb.co/W2gg89z/12.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/S3mSzxL/5.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/4fM3892/6.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/fHmNCcc/7.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/CM3jyK6/8.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/9r3BrpD/9.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/HdGsN55/10.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/jMdBRQ5/11.jpg"></expandable-image>
+        <expandable-image src="https://ibb.co/mc66Zsy/12.jpg"></expandable-image>
       </div>
     </div>
   </page-layout>
